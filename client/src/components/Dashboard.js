1e2823e9d969b794b257935fcce5f2da25ed033d
KirkNotCaptain <cmariekirk@gmail.com>
2021-05-02 19:22:39 -0400

    useEffect with clearInterval to fix re-rendering bug

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,208 +1,222 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import PublicIcon from '@material-ui/icons/Public';
-import WarningIcon from '@material-ui/icons/Warning';
-import Map from './Map.js';
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PublicIcon from "@material-ui/icons/Public";
+import WarningIcon from "@material-ui/icons/Warning";
+import Map from "./Map.js";
 // import SolarFieldsList from './SolarFieldsList.js';
-import SFContext from '../context.js';
-import axios from 'axios';
+import SFContext from "../context.js";
+import axios from "axios";
 
 const drawerWidth = 300;
 
 //--------------- MATERIAL UI STYLES -----------///
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
+	root: {
+		display: "flex",
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(["width", "margin"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(["width", "margin"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	menuButton: {
+		marginRight: 36,
+	},
+	hide: {
+		display: "none",
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: "nowrap",
+	},
+	drawerOpen: {
+		width: drawerWidth,
+		transition: theme.transitions.create("width", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	drawerClose: {
+		transition: theme.transitions.create("width", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		overflowX: "hidden",
+		width: theme.spacing(7) + 1,
+		[theme.breakpoints.up("sm")]: {
+			width: theme.spacing(9) + 1,
+		},
+	},
+	toolbar: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "flex-end",
+		padding: theme.spacing(0, 1),
+		// necessary for content to be below app bar
+		...theme.mixins.toolbar,
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+	},
 }));
 
 //---------------    DASHBOARD COMPONENT    -----------///
 
 export default function Dashboard() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
-  const [solarFieldData, setSolarFieldData] = useState({});
-  const [counter, setCounter] = useState(0);
+	const classes = useStyles();
+	const theme = useTheme();
+	const [open, setOpen] = useState(false);
+	const [solarFieldData, setSolarFieldData] = useState({});
+	const [counter, setCounter] = useState(0);
 
-  const changeSolarFieldData = (data) => {
-    setSolarFieldData(data);
-  };
+	const changeSolarFieldData = (data) => {
+		setSolarFieldData(data);
+	};
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
 
-  const fetchSolarField = (solarFieldsID) => {
-    axios
-      .get(
-        `http://localhost:3000/api/v1/solar_farms/${solarFieldsID}/technicians`
-      )
-      .then((data) => {
-        changeSolarFieldData(data.data);
-      });
-  };
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (counter === 16) {
+				setCounter(0);
+			}
+			fetchSolarField(counter);
+			setCounter(counter + 1);
+		}, 3000);
 
-  setInterval(() => {
-    if (counter === 16) {
-      setCounter(0);
-    }
-    fetchSolarField(counter);
-    setCounter(counter + 1);
-  }, 10000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, [solarFieldData]);
 
-  return (
-    <SFContext.Provider
-      value={{
-        counter,
-        solarFieldData,
-        changeSolarFieldData,
-      }}
-    >
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, {
-                [classes.hide]: open,
-              })}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              Solar Fields Technicians
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
-          }}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            <ListItem button key={'Map'}>
-              <ListItemIcon>
-                <PublicIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Map'} />
-            </ListItem>
-            <ListItem button key={'Alerts'}>
-              <ListItemIcon>
-                <WarningIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Alerts'} />
-            </ListItem>
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Map />
-        </main>
-      </div>
-    </SFContext.Provider>
-  );
+	const fetchSolarField = (solarFieldsID) => {
+		axios
+			.get(
+				`http://localhost:3000/api/v1/solar_farms/${solarFieldsID}/technicians`
+			)
+			.then((data) => {
+				changeSolarFieldData(data.data);
+			});
+	};
+
+	// setInterval(() => {
+	//   if (counter === 16) {
+	//     setCounter(0);
+	//   }
+	//   fetchSolarField(counter);
+	//   setCounter(counter + 1);
+	// }, 10000);
+
+	return (
+		<SFContext.Provider
+			value={{
+				counter,
+				solarFieldData,
+				changeSolarFieldData,
+			}}
+		>
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar
+					position="fixed"
+					className={clsx(classes.appBar, {
+						[classes.appBarShift]: open,
+					})}
+				>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={handleDrawerOpen}
+							edge="start"
+							className={clsx(classes.menuButton, {
+								[classes.hide]: open,
+							})}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" noWrap>
+							Solar Fields Technicians
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="permanent"
+					className={clsx(classes.drawer, {
+						[classes.drawerOpen]: open,
+						[classes.drawerClose]: !open,
+					})}
+					classes={{
+						paper: clsx({
+							[classes.drawerOpen]: open,
+							[classes.drawerClose]: !open,
+						}),
+					}}
+				>
+					<div className={classes.toolbar}>
+						<IconButton onClick={handleDrawerClose}>
+							{theme.direction === "rtl" ? (
+								<ChevronRightIcon />
+							) : (
+								<ChevronLeftIcon />
+							)}
+						</IconButton>
+					</div>
+					<Divider />
+					<List>
+						<ListItem button key={"Map"}>
+							<ListItemIcon>
+								<PublicIcon />
+							</ListItemIcon>
+							<ListItemText primary={"Map"} />
+						</ListItem>
+						<ListItem button key={"Alerts"}>
+							<ListItemIcon>
+								<WarningIcon />
+							</ListItemIcon>
+							<ListItemText primary={"Alerts"} />
+						</ListItem>
+					</List>
+				</Drawer>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					<Map />
+				</main>
+			</div>
+		</SFContext.Provider>
+	);
 }
